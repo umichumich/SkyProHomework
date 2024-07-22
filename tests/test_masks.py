dd04e3c03c12eb1c6def3027f47296fb36ebcd5d
Alexu <AlexusUmich@yandex.ru>
2024-07-22 21:42:54 +0300

    add new func in tests

---
+++ tests/test_masks.py
@@ -0,0 +1,7 @@
+from src.masks import get_mask_card_number, get_mask_account
+
+def test_get_mask_card_number():
+    pass
+
+def test_get_mask_account():
+    pass
---
+++ tests/test_widget.py
@@ -0,0 +1,7 @@
+from src.widget import mask_account_card, get_date
+
+def test_mask_account_card():
+    pass
+
+def test_get_date():
+    pass
