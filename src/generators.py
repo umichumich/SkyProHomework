5e60c75624c0a5a4d5460ba924d33242081b7dc8
Alexu <AlexusUmich@yandex.ru>
2024-08-28 19:31:59 +0300

    add func in generators.py and test_generators.py

--- src/generators.py
+++ src/generators.py
@@ -1,2 +1,7 @@
-def filter_by_currency():
-    pass
+def filter_by_currency(transactions_list, currency):
+    if len(transactions_list) > 0:
+        filtered_transactions = filter(
+            lambda transactions_list: transactions_list.get("operationAmount").get("currency").get("code") == currency, transactions_list)
+        return filtered_transactions
+    else:
+        return "Список пустой!"
--- tests/conftest.py
+++ tests/conftest.py
@@ -32,3 +32,45 @@ def list_state_2():
         {"id": 594226727, "state": "EXECUTED", "date": "2018-09-12T21:27:25.241689"},
         {"id": 615064591, "state": "EXECUTED", "date": "2018-10-14T08:21:33.419441"},
     ]
+
+
+@pytest.fixture
+def transactions():
+    return [{
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {
+            "amount": "9824.07",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702"
+    },
+        {
+            "id": 142264268,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {
+                "amount": "79114.93",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188"
+        }
+    ]
+
+
+def test_filter_by_currency_exceptions(transactions):
+    result = filter_by_currency(transactions, "EUR")
+    assert list(result) == []
+    result = filter_by_currency([], "EUR")
+    assert result == "Список пустой!"
