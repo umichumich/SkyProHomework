c9a201ed34f49fa24fcb843e5ecd653378808705
Alexu <AlexusUmich@yandex.ru>
2024-08-30 19:31:20 +0300

    del fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -69,8 +69,3 @@ def transactions():
     ]
 
 
-def test_filter_by_currency_exceptions(transactions):
-    result = filter_by_currency(transactions, "EUR")
-    assert list(result) == []
-    result = filter_by_currency([], "EUR")
-    assert result == "Список пустой!"
