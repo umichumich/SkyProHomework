f2222eb6db373e4ecba710048b5525b41b224016
Alexu <AlexusUmich@yandex.ru>
2024-08-31 12:11:44 +0300

    add func in decorators.py

--- src/decorators.py
+++ src/decorators.py
@@ -1,2 +1,30 @@
-def log():
-    pass
+from functools import wraps
+from typing import Any, Callable
+
+
+def log(filename: Any) -> Callable:
+    def decorator(func: Callable) -> Callable:
+        @wraps(func)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            try:
+                result = func(*args, **kwargs)
+                log_message = f"{func.__name__} called with args: {args}, kwargs: {kwargs}. Result: {result}"
+                with open(filename, "a") as f:
+                    f.write(log_message + "\n")
+                print(log_message)
+            except Exception as e:
+                error_message = f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}"
+                with open(filename, "a") as f:
+                    f.write(error_message + "\n")
+                print(error_message)
+            return wrapper
+
+        return decorator
+
+
+@log(filename="test_log.txt")
+def my_function(x: int, y: int) -> int:
+    return x + y
+
+
+my_function(1, "t")
