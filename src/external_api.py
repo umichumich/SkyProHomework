616bf217abe5623192051ef2272f112d0d43d758
Alexu <AlexusUmich@yandex.ru>
2024-09-07 23:30:47 +0300

    create external_api.py and .env

---
+++ src/external_api.py
@@ -0,0 +1,19 @@
+from typing import Any
+
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+values = os.getenv("API_KEY")
+
+def currency_conversion(transaction: Any) -> Any:
+    """Функция конвертации"""
+    amount = transaction["operationAmount"]["amount"]
+    code = transaction["operationAmount"]["currency"]["code"]
+    to = "RUB"
+    url = f"https://api.apilayer.com/exchangerates_data/convert?to={to}&from={code}&amount={amount}"
+    payload = {}
+    response = requests.get(url, headers={"API_KEY": values}, data=payload)
+    result = response.json()
+    return result["result"]
