b9afc6b520c6dee065dcebe8d5522979d3c706e8
Alexu <AlexusUmich@yandex.ru>
2024-09-07 20:09:45 +0300

    create utils.py and new func

---
+++ src/utils.py
@@ -0,0 +1,28 @@
+import json
+from json import JSONDecodeError
+from typing import Any
+
+from src.external_api import currency_conversion
+
+def financial_transactions(path: str) -> list:
+    """Функция принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях."""
+    try:
+        with open(path, encoding="utf-8") as finacial_file:
+            try:
+                transactions = json.load(finacial_file)
+            except JSONDecodeError:
+                return []
+        if not isinstance(transactions, list):
+            return []
+        return transactions
+    except JSONDecodeError:
+        return []
+
+
+def transaction_amount(trans: dict, currency: str = "RUB") -> Any:
+    """Функция принимает на вход транзакцию и возвращает сумму транзакции в рублях"""
+    if trans["operationAmount"]["currency"]["code"] == currency:
+        amount = trans["operationAmount"]["amount"]
+    else:
+        amount = currency_conversion(trans)
+    return amount
